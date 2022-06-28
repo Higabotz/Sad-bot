c9594c80ef35dd098bef42148a92cdb11c9c952d
Higabotz <107122280+Higabotz@users.noreply.github.com>
2022-06-28 13:09:42 +0700

    Add files via upload

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   let url = 'https://www.youtube.com/watch?v=' + videoId
   let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   let capt = `*Title:* ${title}\n*Published:* ${publishedTime}\n*Duration:* ${durationH}\n*Views:* ${viewH}\n*Url:* ${url}`
-  let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${url}` }]
+  let buttons = [{ buttonText: { displayText: 'thanks😙' }, buttonId: 'hehe' }]
   let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: '_Audio on progress..._', buttons }, { quoted: m })
   // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
   conn.sendMessage(m.chat, { audio: { url: ytLink }, mimetype: 'audio/mpeg' }, { quoted: msg })
