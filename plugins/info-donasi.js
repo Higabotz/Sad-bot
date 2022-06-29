5d87dc97027c5156e2606fdbc4f4a71f303a12cb
Higabotz <107122280+Higabotz@users.noreply.github.com>
2022-06-29 22:26:49 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,11 +2,16 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command, usedPrefix }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = ` ${conn.getName(m.sender)} Want Support Bot?
+let str = ` ${conn.getName(m.sender)} Ingin Support Bot?
+kalo mau silahkan aja
+pembayaran bisa memakai cara dibawah
+
 *PAYMENT ↓*
 _*Pulsa/pulse(Telkomsel):*_ 082136421665
 _*Dana/ovo:*_ 082136421665
 Setelah melakukan donasi kirim bukti pembayaran ke owner
+
+Terima kasih buat yg udh mau doanasi🤗
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://youtube.com/channel/UCNr667OTz0M7kvfdffWasUw', 'youtube', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
